5a3e8392cf610134aa7efbef2a6cc7d04c1b9888
moeshin <764686875@qq.com>
2020-05-18 09:42:38 +0800

    发布 2.0.3

--- Plugin.php
+++ Plugin.php
@@ -12,7 +12,7 @@ use QPlayer\Cache\Cache;
  *
  * @package QPlayer2
  * @author Moeshin
- * @version 2.0.2
+ * @version 2.0.3
  * @link https://github.com/moeshin/QPlayer2-Typecho
  */
 class QPlayer2_Plugin extends Typecho_Widget implements Typecho_Plugin_Interface
