37ac16dd2c339b7d1513a8473168107a90ccb18f
MoeShin <hi@moeshin.com>
2021-02-23 04:14:34 +0800

    发布 2.0.9

--- Plugin.php
+++ Plugin.php
@@ -15,7 +15,7 @@ use QPlayer\General_Config;
  *
  * @package QPlayer2
  * @author MoeShin
- * @version 2.0.8
+ * @version 2.0.9
  * @link https://github.com/moeshin/QPlayer2-Typecho
  */
 class QPlayer2_Plugin extends Typecho_Widget implements Typecho_Plugin_Interface
